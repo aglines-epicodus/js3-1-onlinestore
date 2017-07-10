a0cd61ed3c964f91ae2535bf84979bfe56a0e0b5
Andrew Glines and David Wilson <andrew+david@>
2017-07-10 11:48:29 -0700

    create service to pass data into album detail pages; retrieve data dynamically

---
+++ src/app/mock-albums.ts
@@ -0,0 +1,8 @@
+import { Album } from './album.model';
+
+export const ALBUMS: Album[] =
+[
+  new Album("AlbumX", "XTC", "descriptionX", 1),
+  new Album("AlbumY", "Yaz", "descY", 2),
+  new Album("AlbumZ", "ZZtop", "descZ", 3)
+];
